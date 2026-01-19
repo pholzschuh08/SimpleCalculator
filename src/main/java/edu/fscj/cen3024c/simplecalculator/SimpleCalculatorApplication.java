d5b92ab9ce42293837216d1e6b67d6c63465f925
ethenhetz <143417055+ethenhetz@users.noreply.github.com>
2026-01-18 21:58:07 -0500

    Add files via upload

--- src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleCalculatorApplication.java
+++ src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleCalculatorApplication.java
@@ -1,7 +1,9 @@
 // SimpleCalculatorApplication.java
 // D. Singletary
 // 1/9/2024
-// Template for calculator application
+// Student ID: s3744461
+// Ethen Hetz
+//1/18/2026
 
 package edu.fscj.cen3024c.simplecalculator;
 
@@ -10,12 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleCalculatorApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SimpleCalculatorApplication.class, args);
 
         // Test the multiply function
         int number1 = 10; // This can be input from the user or arguments
-        int number2 = 5; // This can be input from the user or arguments
+        int number2 = 5;  // This can be input from the user or arguments
         int result = multiply(number1, number2);
         System.out.println("Multiply result is: " + result);
 
@@ -30,5 +33,25 @@ public class SimpleCalculatorApplication {
         // Test the subtract function
         result = subtract(number1, number2);
         System.out.println("Add result is: " + result);
+    }
+
+    // Adds two integers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtracts two integers
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiplies two integers
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Divides two integers
+    public static int divide(int a, int b) {
+        return a / b;
     }
 }
